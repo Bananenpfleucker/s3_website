39b497d9161f66445a10623f5dedc04ad4e7f03f
Checklet <ac.cekic@outlook.com>
2025-03-13 23:02:22 +0100

    Added: Home page with basic search

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,21 @@ import { JSX } from "react";
 export function Home(): JSX.Element {
     return (
         <>
+            <div className="card">
+                <h1>Leitliniensuche</h1>
 
+                <div className="form-merge">
+                    <input type="text" placeholder="Suchbegriff" />
+
+                    <button>
+                        Suchen →
+                    </button>
+
+                    <button>
+                        Erweitert →
+                    </button>
+                </div>
+            </div>
         </>
     );
 };
