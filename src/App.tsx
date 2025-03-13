db1d9b2a93537045bea84ca7a8d35e14d7f1aa7c
Checklet <ac.cekic@outlook.com>
2025-03-13 23:01:49 +0100

    Added: Routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,28 @@ import { JSX } from "react";
 import { Footer } from "./comp/Footer";
 import { Header } from "./comp/Header";
 import { Home } from "./pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { CheatSheet } from "./pages/Cheatsheet";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        Component: Home
+    },
+    {
+        path: '/design-test',
+        Component: CheatSheet
+    }
+]);
 
 export default function App(): JSX.Element {
     return (
         <>
             <Header />
 
-            <Home />
+            <main id="page">
+                <RouterProvider router={router} />
+            </main>
 
             <Footer />
         </>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,53 +2,8 @@ import { JSX } from "react";
 
 export function Home(): JSX.Element {
     return (
-        <main id="main-page">
-            <h1>Heading 1</h1>
+        <>
 
-            <h2>Heading 2</h2>
-
-            <h3>Heading 3</h3>
-
-            <h4>Heading 4</h4>
-
-            <p>
-                Lorem ipsum odor amet, consectetuer adipiscing elit. Et hendrerit imperdiet morbi dignissim turpis
-                eu vehicula maecenas. Eros posuere sapien sit duis nostra ornare ut augue. Interdum blandit est urna
-                maximus, aliquet consequat. Fames penatibus parturient varius eleifend duis. Imperdiet placerat quam
-                porttitor arcu natoque congue libero. Conubia nascetur in parturient fermentum turpis venenatis
-                accumsan himenaeos?
-            </p>
-
-            <p>
-                Himenaeos lacinia nam condimentum consequat velit malesuada est accumsan? Gravida rutrum elementum vulputate
-                euismod sollicitudin. Cras scelerisque in, dictum senectus torquent id accumsan. Vitae quam eros; interdum
-                accumsan quisque consequat laoreet sapien. Efficitur class sagittis ex vestibulum varius placerat auctor velit.
-                Hac senectus sit auctor in eu eu. Congue ligula morbi nulla sapien ut suscipit. Ridiculus proin tristique
-                semper augue elementum. Libero viverra adipiscing per adipiscing fringilla blandit.
-            </p>
-
-            <p>
-                Taciti ad turpis parturient phasellus congue rutrum. Id elit lacinia; aliquet hac hac id. Rutrum hac natoque
-                ullamcorper bibendum egestas. Facilisis blandit maximus eget torquent vivamus molestie tempor maecenas arcu.
-                Leo maecenas rutrum nulla per litora imperdiet velit dictum. Maecenas vestibulum laoreet hac lectus, habitant
-                in faucibus pulvinar. Dis sollicitudin et sit penatibus non est hac.
-            </p>
-
-            <p>
-                Habitasse sem consequat platea accumsan proin integer. Ullamcorper ad vulputate pretium volutpat imperdiet.
-                Egestas viverra est sollicitudin inceptos scelerisque quam placerat adipiscing varius. Lobortis ligula leo
-                ut purus montes habitasse fusce. Purus facilisis dignissim vehicula; venenatis tincidunt odio tincidunt duis.
-                Pharetra venenatis fermentum posuere cubilia metus netus senectus. Dictumst dignissim mi facilisis iaculis;
-                sit in. Proin pharetra scelerisque in ornare torquent nec a. Pretium sed cubilia vel penatibus curae.
-            </p>
-
-            <p>
-                Posuere venenatis dis vestibulum morbi risus aliquam. Finibus bibendum ut imperdiet curae rutrum tortor
-                dignissim dictum sollicitudin. Maecenas himenaeos facilisis gravida commodo fames fermentum ridiculus leo metus.
-                Gravida egestas euismod congue nascetur dictum purus. Dapibus est faucibus sagittis nostra tincidunt dignissim
-                turpis id. Ante eros inceptos facilisi lacinia litora netus posuere nam at. Elit pulvinar nec suscipit feugiat vel.
-                Est orci magna mi senectus quis molestie aliquam placerat. Donec est risus justo mauris conubia.
-            </p>
-        </main>
+        </>
     );
 };
