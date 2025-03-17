bf008d76c0ec7b991afb6082ade0d01424773366
Checklet <ac.cekic@outlook.com>
2025-03-17 23:24:12 +0100

    Updated: Compiled files.

--- docs/assets/index-hw44ScZ_.js
+++ docs/assets/index-hw44ScZ_.js
@@ -35,30 +35,6 @@
     fetch(link.href, fetchOpts);
   }
 })();
-function getAugmentedNamespace(n) {
-  if (n.__esModule) return n;
-  var f = n.default;
-  if (typeof f == "function") {
-    var a = function a2() {
-      if (this instanceof a2) {
-        return Reflect.construct(f, arguments, this.constructor);
-      }
-      return f.apply(this, arguments);
-    };
-    a.prototype = f.prototype;
-  } else a = {};
-  Object.defineProperty(a, "__esModule", { value: true });
-  Object.keys(n).forEach(function(k) {
-    var d = Object.getOwnPropertyDescriptor(n, k);
-    Object.defineProperty(a, k, d.get ? d : {
-      enumerable: true,
-      get: function() {
-        return n[k];
-      }
-    });
-  });
-  return a;
-}
 var jsxRuntime = { exports: {} };
 var reactJsxRuntime_production = {};
 /**
@@ -75,21 +51,21 @@ function requireReactJsxRuntime_production() {
   if (hasRequiredReactJsxRuntime_production) return reactJsxRuntime_production;
   hasRequiredReactJsxRuntime_production = 1;
   var REACT_ELEMENT_TYPE = Symbol.for("react.transitional.element"), REACT_FRAGMENT_TYPE = Symbol.for("react.fragment");
-  function jsxProd(type, config2, maybeKey) {
+  function jsxProd(type, config, maybeKey) {
     var key = null;
     void 0 !== maybeKey && (key = "" + maybeKey);
-    void 0 !== config2.key && (key = "" + config2.key);
-    if ("key" in config2) {
+    void 0 !== config.key && (key = "" + config.key);
+    if ("key" in config) {
       maybeKey = {};
-      for (var propName in config2)
-        "key" !== propName && (maybeKey[propName] = config2[propName]);
-    } else maybeKey = config2;
-    config2 = maybeKey.ref;
+      for (var propName in config)
+        "key" !== propName && (maybeKey[propName] = config[propName]);
+    } else maybeKey = config;
+    config = maybeKey.ref;
     return {
       $$typeof: REACT_ELEMENT_TYPE,
       type,
       key,
-      ref: void 0 !== config2 ? config2 : null,
+      ref: void 0 !== config ? config : null,
       props: maybeKey
     };
   }
@@ -393,15 +369,15 @@ function requireReact_production() {
       return fn.apply(null, arguments);
     };
   };
-  react_production.cloneElement = function(element, config2, children) {
+  react_production.cloneElement = function(element, config, children) {
     if (null === element || void 0 === element)
       throw Error(
         "The argument must be a React element, but you passed " + element + "."
       );
     var props = assign({}, element.props), key = element.key, owner = void 0;
-    if (null != config2)
-      for (propName in void 0 !== config2.ref && (owner = void 0), void 0 !== config2.key && (key = "" + config2.key), config2)
-        !hasOwnProperty.call(config2, propName) || "key" === propName || "__self" === propName || "__source" === propName || "ref" === propName && void 0 === config2.ref || (props[propName] = config2[propName]);
+    if (null != config)
+      for (propName in void 0 !== config.ref && (owner = void 0), void 0 !== config.key && (key = "" + config.key), config)
+        !hasOwnProperty.call(config, propName) || "key" === propName || "__self" === propName || "__source" === propName || "ref" === propName && void 0 === config.ref || (props[propName] = config[propName]);
     var propName = arguments.length - 2;
     if (1 === propName) props.children = children;
     else if (1 < propName) {
@@ -427,11 +403,11 @@ function requireReact_production() {
     };
     return defaultValue;
   };
-  react_production.createElement = function(type, config2, children) {
+  react_production.createElement = function(type, config, children) {
     var propName, props = {}, key = null;
-    if (null != config2)
-      for (propName in void 0 !== config2.key && (key = "" + config2.key), config2)
-        hasOwnProperty.call(config2, propName) && "key" !== propName && "__self" !== propName && "__source" !== propName && (props[propName] = config2[propName]);
+    if (null != config)
+      for (propName in void 0 !== config.key && (key = "" + config.key), config)
+        hasOwnProperty.call(config, propName) && "key" !== propName && "__self" !== propName && "__source" !== propName && (props[propName] = config[propName]);
     var childrenLength = arguments.length - 2;
     if (1 === childrenLength) props.children = children;
     else if (1 < childrenLength) {
@@ -17462,345 +17438,6 @@ function useViewTransitionState(to, opts = {}) {
   return matchPath(path.pathname, nextPath) != null || matchPath(path.pathname, currentPath) != null;
 }
 new TextEncoder();
-var config = {};
-var main = { exports: {} };
-const __viteBrowserExternal = {};
-const __viteBrowserExternal$1 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
-  __proto__: null,
-  default: __viteBrowserExternal
-}, Symbol.toStringTag, { value: "Module" }));
-const require$$3 = /* @__PURE__ */ getAugmentedNamespace(__viteBrowserExternal$1);
-const version = "16.4.7";
-const require$$4 = {
-  version
-};
-var hasRequiredMain;
-function requireMain() {
-  if (hasRequiredMain) return main.exports;
-  hasRequiredMain = 1;
-  var define_process_env_default2 = {};
-  const fs = require$$3;
-  const path = require$$3;
-  const os = require$$3;
-  const crypto = require$$3;
-  const packageJson = require$$4;
-  const version2 = packageJson.version;
-  const LINE = /(?:^|^)\s*(?:export\s+)?([\w.-]+)(?:\s*=\s*?|:\s+?)(\s*'(?:\\'|[^'])*'|\s*"(?:\\"|[^"])*"|\s*`(?:\\`|[^`])*`|[^#\r\n]+)?\s*(?:#.*)?(?:$|$)/mg;
-  function parse(src) {
-    const obj = {};
-    let lines = src.toString();
-    lines = lines.replace(/\r\n?/mg, "\n");
-    let match;
-    while ((match = LINE.exec(lines)) != null) {
-      const key = match[1];
-      let value = match[2] || "";
-      value = value.trim();
-      const maybeQuote = value[0];
-      value = value.replace(/^(['"`])([\s\S]*)\1$/mg, "$2");
-      if (maybeQuote === '"') {
-        value = value.replace(/\\n/g, "\n");
-        value = value.replace(/\\r/g, "\r");
-      }
-      obj[key] = value;
-    }
-    return obj;
-  }
-  function _parseVault(options) {
-    const vaultPath = _vaultPath(options);
-    const result = DotenvModule.configDotenv({ path: vaultPath });
-    if (!result.parsed) {
-      const err = new Error(`MISSING_DATA: Cannot parse ${vaultPath} for an unknown reason`);
-      err.code = "MISSING_DATA";
-      throw err;
-    }
-    const keys = _dotenvKey(options).split(",");
-    const length = keys.length;
-    let decrypted;
-    for (let i = 0; i < length; i++) {
-      try {
-        const key = keys[i].trim();
-        const attrs = _instructions(result, key);
-        decrypted = DotenvModule.decrypt(attrs.ciphertext, attrs.key);
-        break;
-      } catch (error) {
-        if (i + 1 >= length) {
-          throw error;
-        }
-      }
-    }
-    return DotenvModule.parse(decrypted);
-  }
-  function _log(message) {
-    console.log(`[dotenv@${version2}][INFO] ${message}`);
-  }
-  function _warn(message) {
-    console.log(`[dotenv@${version2}][WARN] ${message}`);
-  }
-  function _debug(message) {
-    console.log(`[dotenv@${version2}][DEBUG] ${message}`);
-  }
-  function _dotenvKey(options) {
-    if (options && options.DOTENV_KEY && options.DOTENV_KEY.length > 0) {
-      return options.DOTENV_KEY;
-    }
-    if (define_process_env_default2.DOTENV_KEY && define_process_env_default2.DOTENV_KEY.length > 0) {
-      return define_process_env_default2.DOTENV_KEY;
-    }
-    return "";
-  }
-  function _instructions(result, dotenvKey) {
-    let uri;
-    try {
-      uri = new URL(dotenvKey);
-    } catch (error) {
-      if (error.code === "ERR_INVALID_URL") {
-        const err = new Error("INVALID_DOTENV_KEY: Wrong format. Must be in valid uri format like dotenv://:key_1234@dotenvx.com/vault/.env.vault?environment=development");
-        err.code = "INVALID_DOTENV_KEY";
-        throw err;
-      }
-      throw error;
-    }
-    const key = uri.password;
-    if (!key) {
-      const err = new Error("INVALID_DOTENV_KEY: Missing key part");
-      err.code = "INVALID_DOTENV_KEY";
-      throw err;
-    }
-    const environment = uri.searchParams.get("environment");
-    if (!environment) {
-      const err = new Error("INVALID_DOTENV_KEY: Missing environment part");
-      err.code = "INVALID_DOTENV_KEY";
-      throw err;
-    }
-    const environmentKey = `DOTENV_VAULT_${environment.toUpperCase()}`;
-    const ciphertext = result.parsed[environmentKey];
-    if (!ciphertext) {
-      const err = new Error(`NOT_FOUND_DOTENV_ENVIRONMENT: Cannot locate environment ${environmentKey} in your .env.vault file.`);
-      err.code = "NOT_FOUND_DOTENV_ENVIRONMENT";
-      throw err;
-    }
-    return { ciphertext, key };
-  }
-  function _vaultPath(options) {
-    let possibleVaultPath = null;
-    if (options && options.path && options.path.length > 0) {
-      if (Array.isArray(options.path)) {
-        for (const filepath of options.path) {
-          if (fs.existsSync(filepath)) {
-            possibleVaultPath = filepath.endsWith(".vault") ? filepath : `${filepath}.vault`;
-          }
-        }
-      } else {
-        possibleVaultPath = options.path.endsWith(".vault") ? options.path : `${options.path}.vault`;
-      }
-    } else {
-      possibleVaultPath = path.resolve(process.cwd(), ".env.vault");
-    }
-    if (fs.existsSync(possibleVaultPath)) {
-      return possibleVaultPath;
-    }
-    return null;
-  }
-  function _resolveHome(envPath) {
-    return envPath[0] === "~" ? path.join(os.homedir(), envPath.slice(1)) : envPath;
-  }
-  function _configVault(options) {
-    _log("Loading env from encrypted .env.vault");
-    const parsed = DotenvModule._parseVault(options);
-    let processEnv = define_process_env_default2;
-    if (options && options.processEnv != null) {
-      processEnv = options.processEnv;
-    }
-    DotenvModule.populate(processEnv, parsed, options);
-    return { parsed };
-  }
-  function configDotenv(options) {
-    const dotenvPath = path.resolve(process.cwd(), ".env");
-    let encoding = "utf8";
-    const debug = Boolean(options && options.debug);
-    if (options && options.encoding) {
-      encoding = options.encoding;
-    } else {
-      if (debug) {
-        _debug("No encoding is specified. UTF-8 is used by default");
-      }
-    }
-    let optionPaths = [dotenvPath];
-    if (options && options.path) {
-      if (!Array.isArray(options.path)) {
-        optionPaths = [_resolveHome(options.path)];
-      } else {
-        optionPaths = [];
-        for (const filepath of options.path) {
-          optionPaths.push(_resolveHome(filepath));
-        }
-      }
-    }
-    let lastError;
-    const parsedAll = {};
-    for (const path2 of optionPaths) {
-      try {
-        const parsed = DotenvModule.parse(fs.readFileSync(path2, { encoding }));
-        DotenvModule.populate(parsedAll, parsed, options);
-      } catch (e) {
-        if (debug) {
-          _debug(`Failed to load ${path2} ${e.message}`);
-        }
-        lastError = e;
-      }
-    }
-    let processEnv = define_process_env_default2;
-    if (options && options.processEnv != null) {
-      processEnv = options.processEnv;
-    }
-    DotenvModule.populate(processEnv, parsedAll, options);
-    if (lastError) {
-      return { parsed: parsedAll, error: lastError };
-    } else {
-      return { parsed: parsedAll };
-    }
-  }
-  function config2(options) {
-    if (_dotenvKey(options).length === 0) {
-      return DotenvModule.configDotenv(options);
-    }
-    const vaultPath = _vaultPath(options);
-    if (!vaultPath) {
-      _warn(`You set DOTENV_KEY but you are missing a .env.vault file at ${vaultPath}. Did you forget to build it?`);
-      return DotenvModule.configDotenv(options);
-    }
-    return DotenvModule._configVault(options);
-  }
-  function decrypt(encrypted, keyStr) {
-    const key = Buffer.from(keyStr.slice(-64), "hex");
-    let ciphertext = Buffer.from(encrypted, "base64");
-    const nonce = ciphertext.subarray(0, 12);
-    const authTag = ciphertext.subarray(-16);
-    ciphertext = ciphertext.subarray(12, -16);
-    try {
-      const aesgcm = crypto.createDecipheriv("aes-256-gcm", key, nonce);
-      aesgcm.setAuthTag(authTag);
-      return `${aesgcm.update(ciphertext)}${aesgcm.final()}`;
-    } catch (error) {
-      const isRange = error instanceof RangeError;
-      const invalidKeyLength = error.message === "Invalid key length";
-      const decryptionFailed = error.message === "Unsupported state or unable to authenticate data";
-      if (isRange || invalidKeyLength) {
-        const err = new Error("INVALID_DOTENV_KEY: It must be 64 characters long (or more)");
-        err.code = "INVALID_DOTENV_KEY";
-        throw err;
-      } else if (decryptionFailed) {
-        const err = new Error("DECRYPTION_FAILED: Please check your DOTENV_KEY");
-        err.code = "DECRYPTION_FAILED";
-        throw err;
-      } else {
-        throw error;
-      }
-    }
-  }
-  function populate(processEnv, parsed, options = {}) {
-    const debug = Boolean(options && options.debug);
-    const override = Boolean(options && options.override);
-    if (typeof parsed !== "object") {
-      const err = new Error("OBJECT_REQUIRED: Please check the processEnv argument being passed to populate");
-      err.code = "OBJECT_REQUIRED";
-      throw err;
-    }
-    for (const key of Object.keys(parsed)) {
-      if (Object.prototype.hasOwnProperty.call(processEnv, key)) {
-        if (override === true) {
-          processEnv[key] = parsed[key];
-        }
-        if (debug) {
-          if (override === true) {
-            _debug(`"${key}" is already defined and WAS overwritten`);
-          } else {
-            _debug(`"${key}" is already defined and was NOT overwritten`);
-          }
-        }
-      } else {
-        processEnv[key] = parsed[key];
-      }
-    }
-  }
-  const DotenvModule = {
-    configDotenv,
-    _configVault,
-    _parseVault,
-    config: config2,
-    decrypt,
-    parse,
-    populate
-  };
-  main.exports.configDotenv = DotenvModule.configDotenv;
-  main.exports._configVault = DotenvModule._configVault;
-  main.exports._parseVault = DotenvModule._parseVault;
-  main.exports.config = DotenvModule.config;
-  main.exports.decrypt = DotenvModule.decrypt;
-  main.exports.parse = DotenvModule.parse;
-  main.exports.populate = DotenvModule.populate;
-  main.exports = DotenvModule;
-  return main.exports;
-}
-var envOptions;
-var hasRequiredEnvOptions;
-function requireEnvOptions() {
-  if (hasRequiredEnvOptions) return envOptions;
-  hasRequiredEnvOptions = 1;
-  var define_process_env_default2 = {};
-  const options = {};
-  if (define_process_env_default2.DOTENV_CONFIG_ENCODING != null) {
-    options.encoding = define_process_env_default2.DOTENV_CONFIG_ENCODING;
-  }
-  if (define_process_env_default2.DOTENV_CONFIG_PATH != null) {
-    options.path = define_process_env_default2.DOTENV_CONFIG_PATH;
-  }
-  if (define_process_env_default2.DOTENV_CONFIG_DEBUG != null) {
-    options.debug = define_process_env_default2.DOTENV_CONFIG_DEBUG;
-  }
-  if (define_process_env_default2.DOTENV_CONFIG_OVERRIDE != null) {
-    options.override = define_process_env_default2.DOTENV_CONFIG_OVERRIDE;
-  }
-  if (define_process_env_default2.DOTENV_CONFIG_DOTENV_KEY != null) {
-    options.DOTENV_KEY = define_process_env_default2.DOTENV_CONFIG_DOTENV_KEY;
-  }
-  envOptions = options;
-  return envOptions;
-}
-var cliOptions;
-var hasRequiredCliOptions;
-function requireCliOptions() {
-  if (hasRequiredCliOptions) return cliOptions;
-  hasRequiredCliOptions = 1;
-  const re = /^dotenv_config_(encoding|path|debug|override|DOTENV_KEY)=(.+)$/;
-  cliOptions = function optionMatcher(args) {
-    return args.reduce(function(acc, cur) {
-      const matches = cur.match(re);
-      if (matches) {
-        acc[matches[1]] = matches[2];
-      }
-      return acc;
-    }, {});
-  };
-  return cliOptions;
-}
-var hasRequiredConfig;
-function requireConfig() {
-  if (hasRequiredConfig) return config;
-  hasRequiredConfig = 1;
-  (function() {
-    requireMain().config(
-      Object.assign(
-        {},
-        requireEnvOptions(),
-        requireCliOptions()(process.argv)
-      )
-    );
-  })();
-  return config;
-}
-requireConfig();
-var define_process_env_default = {};
 const router = createBrowserRouter([
   {
     path: "/",
@@ -17811,7 +17448,7 @@ const router = createBrowserRouter([
     Component: CheatSheetPage
   }
 ], {
-  basename: define_process_env_default.REACT_APP_BASE_NAME
+  basename: "s3_website/"
 });
 function App() {
   return /* @__PURE__ */ jsxRuntimeExports.jsxs(jsxRuntimeExports.Fragment, { children: [
