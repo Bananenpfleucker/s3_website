c9b0b8ea950723c31b3b1a1fe4f444f0ead77f13
Klim-bimm <shevlyga777@gmail.com>
2025-04-01 00:08:02 +0200

    Anpassung des Design  ohne Boxen und Kacheln sowie Hinzufügung eines Videos als background

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -1,22 +1,20 @@
-import { JSX } from "react";
+import { JSX, ReactNode } from "react";
 
 export type CardComponentProps = {
-    children: any,
-    title?: string | undefined
-}
+    children: ReactNode;
+    title?: string;
+    variant?: "default" | "flat";
+};
 
 /**
  * Displays the contents in a card-like element.
- * 
  * A title can be alternatively supplied to the element.
  */
-export default function CardComponent({ children, title }: CardComponentProps): JSX.Element {
+export default function CardComponent({ children, title, variant = "default" }: CardComponentProps): JSX.Element {
     return (
-        <div className="card">
-            {title != undefined && <h2 className="break-word">{title}</h2>}
-
-
+        <div className={`card ${variant === "flat" ? "card-flat" : ""}`}>
+            {title !== undefined && <h2 className="break-word">{title}</h2>}
             {children}
         </div>
     );
-};
+}
